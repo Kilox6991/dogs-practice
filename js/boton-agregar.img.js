b8d944e0eed77afa7263108a4fd6dc55ae407768
Kilox6991 <4inf.mpromero@gmail.com>
2023-06-20 16:25:10 +0200

    Funcion boton casi completa

--- js/boton-agregar.img.js
+++ js/boton-agregar.img.js
@@ -1,32 +1,55 @@
-
 //Evento Boton 
-const newImg = document.getElementById('NewDog')
-const newImgDiv = document.getElementById('grid')
-const razaImg = ["spaniel-blenheim","lhasa","shihtzu"][Math.floor(Math.random()*3)]
+const newImg = document.getElementById('newDog')
+const newImgDiv = document.getElementsByClassName('table')
+console.log(newImgDiv)
 
 
 const botonELDOM = document.getElementById('Btn')
 botonELDOM.addEventListener("click", handleClick)
+
 function handleClick() {
-        fetch ('https://dog.ceo/api/breeds/image/random')
+    fetch('https://dog.ceo/api/breeds/image/random')
         .then((res) => res.json())
-        .then((data) =>{
-            console.log(data)
-            pintarImagen(data.message, newImg)
-            pintarImagen(data.message, newImgDiv)
-            //funcion Vega 
-            // if (condition) {
-                
-            //  }
+        .then((data) => {
 
-    })
-}
+                newImg.innerHTML = "";
+                let srcImg= data.message
+                console.log("ERES TU", srcImg)
+                pintarImagen(srcImg, newImg)
+
+                    
 
 
-//Import Pintar Img
-function pintarImagen(url,parentNode) {
-	const img = document.createElement('img')
-	img.src = url;
-    parentNode.innerHTML = '';
-    parentNode.appendChild(img)
-}
+                for (let i = 0; i < newImgDiv.length; i++) {
+                    if (newImgDiv[i].firstChild === null) {
+                        if (i === newImgDiv.length - 1) {
+                            fetch('https://api.thecatapi.com/v1/images/search')
+                                .then((res) => res.json())
+                                .then((data) => {
+                                    console.log(data[0].url)
+                                    
+                                    srcImg = data[0].url
+                                    console.log("ERES UN GATO", srcImg)
+                                    pintarImagen(srcImg,newImgDiv[i])
+
+                                    newImg.firstChild.src = srcImg
+                                    })                     
+                                }
+                            else {
+                                pintarImagen(srcImg, newImgDiv[i])
+                                
+                            }
+                            break
+                        }
+                    }
+                    console.log(srcImg)
+                })
+        }
+
+
+    //Pintar Img
+    function pintarImagen(url, parentNode) {
+        const img = document.createElement('img')
+        img.src = url;
+        parentNode.appendChild(img)
+    }
